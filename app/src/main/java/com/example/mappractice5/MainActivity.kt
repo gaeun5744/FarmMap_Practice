df77192baf6765a7afbf77fc055ce3b16121121a
gaeun <geun5744@gmail.com>
2022-03-09 21:32:39 +0900

    위성지도로 변환

--- app/src/main/java/com/example/mappractice5/MainActivity.kt
+++ app/src/main/java/com/example/mappractice5/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.mappractice5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.GoogleMapOptions
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class MainActivity : AppCompatActivity() {
+private lateinit var mMap: GoogleMap
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+
+        mapFragment.getMapAsync(this)
+
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        var mMap = googleMap
+        mMap.mapType=GoogleMap.MAP_TYPE_HYBRID
     }
 }
+
+
