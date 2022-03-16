30403aa339d47316343688ed8148ea96ddb89b73
gaeun <geun5744@gmail.com>
2022-03-16 18:21:02 +0900

    클릭 위치 마커로 표시하기

--- app/src/main/java/com/example/mappractice5/MainActivity.kt
+++ app/src/main/java/com/example/mappractice5/MainActivity.kt
@@ -7,8 +7,13 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.GoogleMapOptions
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.maps.android.clustering.ClusterItem
+import com.google.maps.android.clustering.ClusterManager
 
 private lateinit var mMap: GoogleMap
+
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,18 +25,30 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
         mapFragment.getMapAsync(this)
 
+
+
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         var mMap = googleMap
         mMap.mapType=GoogleMap.MAP_TYPE_HYBRID
 
-        mMap.setOnMapLongClickListener { latLng->
-            Toast.makeText(this, "${latLng.latitude},${latLng.longitude}",Toast.LENGTH_SHORT).show()
 
+        mMap.setOnMapLongClickListener { latLng->
+            val location = LatLng(latLng.latitude,latLng.longitude)
+            googleMap.addMarker(MarkerOptions().position(location).title("좌표"). snippet("위도 : ${latLng.latitude}, 경도 : ${latLng.longitude}"))
+
+
+            //Toast.makeText(this, "${latLng.latitude},${latLng.longitude}",Toast.LENGTH_SHORT).show()
         }
 
+
+
     }
+
+
+
 }
+
 
 
