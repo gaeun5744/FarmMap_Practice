846197b68005fbdc151a8c0ba7170a63228a97bc
gaeun <geun5744@gmail.com>
2022-03-23 17:47:35 +0900

    Screenshot 기능 일부 구현

--- app/src/main/java/com/example/mappractice5/MainActivity.kt
+++ app/src/main/java/com/example/mappractice5/MainActivity.kt
@@ -1,10 +1,20 @@
 package com.example.mappractice5
 
+import android.accessibilityservice.AccessibilityService
+import android.content.ContentValues
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
 import androidx.core.content.edit
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.GoogleMapOptions
@@ -16,6 +26,14 @@ import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.google.maps.android.clustering.ClusterItem
 import com.google.maps.android.clustering.ClusterManager
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
+import java.lang.String.format
+import java.text.DateFormat
+import java.text.MessageFormat.format
+import java.util.*
+import kotlin.collections.ArrayList
 
 private lateinit var mMap: GoogleMap
 
@@ -23,6 +41,11 @@ private var size=0
 
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback{
+
+    private val screenShotButton:AppCompatButton by lazy {
+        findViewById<AppCompatButton>(R.id.screenShotButton)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -32,7 +55,85 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback{
 
         mapFragment.getMapAsync(this)
 
+        var v=window.decorView
+
+        screenShotButton.setOnClickListener {
+            val bitmap=getScreenShotFromView(v)
+            if (bitmap!=null){
+                saveMediaToStorage(bitmap)
+            }
+        }
+
+
     }
+
+    private fun getScreenShotFromView(v: View): Bitmap? {
+        // create a bitmap object
+        var screenshot: Bitmap? = null
+        try {
+            // inflate screenshot object
+            // with Bitmap.createBitmap it
+            // requires three parameters
+            // width and height of the view and
+            // the background color
+            screenshot = Bitmap.createBitmap(v.measuredWidth, v.measuredHeight, Bitmap.Config.ARGB_8888)
+            // Now draw this bitmap on a canvas
+            val canvas = Canvas(screenshot)
+            v.draw(canvas)
+        } catch (e: Exception) {
+            Log.e("GFG", "Failed to capture screenshot because:" + e.message)
+        }
+        // return the bitmap
+        return screenshot
+    }
+
+
+    // this method saves the image to gallery
+    private fun saveMediaToStorage(bitmap: Bitmap) {
+        // Generating a file name
+        val filename = "${System.currentTimeMillis()}.jpg"
+
+        // Output stream
+        var fos: OutputStream? = null
+
+        // For devices running android >= Q
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            // getting the contentResolver
+            this.contentResolver?.also { resolver ->
+
+                // Content resolver will process the contentvalues
+                val contentValues = ContentValues().apply {
+
+                    // putting file information in content values
+                    put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+                    put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
+                    put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                }
+
+                // Inserting the contentValues to
+                // contentResolver and getting the Uri
+                val imageUri: Uri? = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
+                // Opening an outputstream with the Uri that we got
+                fos = imageUri?.let { resolver.openOutputStream(it) }
+            }
+        } else {
+            // These for devices running on android < Q
+            val imagesDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            val image = File(imagesDir, filename)
+            fos = FileOutputStream(image)
+        }
+
+        fos?.use {
+            // Finally writing the bitmap to the output stream that we opened
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, it)
+            Toast.makeText(this , "Captured View and saved to Gallery" , Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+
+
 
     override fun onMapReady(googleMap: GoogleMap) {
         fun setStringArrayPref(key: String, values: ArrayList<locationArray>) {
